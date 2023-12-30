d59aa1e043c38e2c3d042f850ea9228ab8bb73e7
math-queiroz <m-alves7@hotmail.com>
2023-12-29 22:38:33 -0300

    feat: disabled virtual text

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,3 +2,14 @@ require('lsp.rust-analyzer')
 require('lsp.yamlls')
 
 require('lspconfig.ui.windows').default_options.border = 'single'
+
+vim.diagnostic.config({
+  virtual_text = false,
+	float = {
+		border = 'rounded'
+	}
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
