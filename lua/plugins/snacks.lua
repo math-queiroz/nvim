039d351f3f72483a6585ce11b17685b316458e64
matheus.queiroz <matheus.queiroz@autbank.com.br>
2025-03-24 13:45:00 -0300

    chore: removed scroll animations and indent

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,11 +5,9 @@ M.lazy = false
 M.opts = {
   animate = { enabled = true },
   bigfile = { enabled = true },
-  indent = { enabled = true },
   input = { enabled = true },
   notifier = { enabled = true },
   quickfile = { enabled = true },
-  scroll = { enabled = true },
 }
 
 return M
